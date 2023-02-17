3e07290d73fe6eff303f1dac736487c8d42ecb84
Shyamleeacholkar <shamleeacholkar38@gmail.com>
2023-02-17 14:19:27 +0530

    AngularSpringboot

--- spring-boot-server/src/main/java/com/spring/datajpa/SpringBootDataJpaApplication.java
+++ spring-boot-server/src/main/java/com/spring/datajpa/SpringBootDataJpaApplication.java
@@ -1,4 +1,4 @@
-package com.bezkoder.spring.datajpa;
+package com.spring.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +8,7 @@ public class SpringBootDataJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
+		System.out.println("Tutorial System is Running....");
 	}
 
 }
